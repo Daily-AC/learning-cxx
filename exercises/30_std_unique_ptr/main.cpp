bdf77bc44ea57b7451c720f497c483d204eb68a3
__zyl_ <2908453829@qq.com>
2024-12-16 18:51:20 +0800

    done

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -53,13 +53,14 @@ int main(int argc, char **argv) {
     drop(drop(reset(drop(reset(reset(nullptr))))));
     problems[2] = std::move(RECORDS);
 
+    //for (auto pr:problems) for (auto s:pr) std::cout << s << '\n';
     // ---- 不要修改以上代码 ----
 
     std::vector<const char *> answers[]{
         {"fd"},
         // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
-        {"ffr", "d"},
-        {"r", "d", "d"},
+        {"d", "ffr"},
+        {"d", "d", "r"},
     };
 
     // ---- 不要修改以下代码 ----
